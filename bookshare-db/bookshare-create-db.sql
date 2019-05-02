27e96c8011c0c4386725120abf00c0ef30e05d2c
Roman <potteroman0@gmail.com>
2019-05-02 20:16:40 +0300

    Added tables to db

--- bookshare-db/bookshare-create-db.sql
+++ bookshare-db/bookshare-create-db.sql
@@ -13,3 +13,28 @@ CREATE TABLE "user"
 	CONSTRAINT uk_email UNIQUE ("email"),
 	CONSTRAINT uk_phone UNIQUE ("phone")
 )
+
+CREATE TABLE "book"
+(
+	"id" SERIAL NOT NULL,
+	"name"	VARCHAR(50) NOT NULL,
+	"autor"	VARCHAR(50) DEFAULT 'Unknow',
+	"genre"	VARCHAR(50) NOT NULL,
+	"publication" VARCHAR(20) NOT NULL,
+	"year"  DATE NOT NULL,
+	"description" VARCHAR(50) DEFAULT 'Not provaided',
+	CONSTRAINT pk_book_id PRIMARY KEY ("id")
+)
+
+CREATE TABLE "announce_board"
+(
+  "id" SERIAL NOT NULL,
+  "user_id" INTEGER NOT NULL,
+  "book_id" INTEGER NOT NULL,
+  "announce_timestamp" TIMESTAMP ,
+  CONSTRAINT pk_announce_board_id PRIMARY KEY ("id"),
+  CONSTRAINT fk_user_id FOREIGN KEY("user_id") 
+  REFERENCES "user"("id"),
+  CONSTRAINT fk_book_id FOREIGN KEY("book_id") 
+  REFERENCES "book"("id")
+);
